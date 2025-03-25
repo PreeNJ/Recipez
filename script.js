73bf566d3462f253dac201b7a15f043edb53ff04
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-26 00:21:37 +0300

    introduces fetch

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
   function searchRecipe() {
     let searchBox = document.getElementById("searchBox").ariaValueMax;
     let apiURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchBox}`;
-    
+
+    // fetch(apiURL)
+    // .then(response = response.json())
+    // .then (data => {
+    //     IdleDeadline(data.meals){
+    //         let meal = data.meals[0];
+    // //     }
+    // })
   }
